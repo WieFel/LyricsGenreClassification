f6e6a4b5c4b6ed28dd3e7cd5b97fe722056a9742
wiefel <felix.wielander@gmail.com>
2019-01-19 12:46:57 +0100

    Adjusted preprocessing Now extracting only english songs

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,9 +1,28 @@
 import pandas
+import pickle
+import langid
 
-csv_data = pandas.read_csv("~/NLP_Data/lyrics.csv")   # read csv data
-data = csv_data[["genre", "lyrics"]]  # only use columns "genre" and "lyrics"
-data = data[data["lyrics"].notnull()]   # filter out empty lyrics
-print(data.head)
 
-# [362237 rows x 2 columns] TOTAL
-# [266557 rows x 2 columns] WITH EMPTY LYRICS FILTERED OUT
+def read_genre_lyrics_data():
+    # read csv data, assuming the lyrics.csv is in ~/NLP_Data/
+    csv_data = pandas.read_csv("/home/felix/NLP_Data/lyrics.csv")
+
+    d = csv_data[["genre", "lyrics"]]  # only use columns "genre" and "lyrics"
+    d = d[d["lyrics"].notnull()]   # filter out empty lyrics
+    d["lyrics"] = d["lyrics"].apply(lambda t: unicode(t, errors="ignore"))   # convert lyrics characters to unicode
+
+    # detect the languages of the lyrics (first 100 chars of lyrics) and add it as new column
+    d["language"] = d["lyrics"].map(lambda t: langid.classify(t[:100])[0])  # -> may take several minutes!
+
+    d = d[(d.language == "en")]  # filter out english songs only
+    d = d[["genre", "lyrics"]]  # again select only the two relevant columns genre and lyrics
+    return d.values
+
+
+
+data = read_genre_lyrics_data()
+
+with open("/home/felix/NLP_Data/dataset.pickle", "wb") as file:
+    pickle.dump(data, file)
+
+print("SUCCESS!")
