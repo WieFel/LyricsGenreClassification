1b54f9b20b456fe341d50695a1c4bf16bde3e25d
WieFel <felix.wielander@gmail.com>
2019-02-15 13:41:44 +0100

    Small correction of comment

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -6,7 +6,7 @@ DATA_PATH = os.path.expanduser("~/NLP_Data/")
 DATASET_FILE = DATA_PATH + "dataset.pickle"
 
 
-# returns data as a np array with first column "genre" and second column "lyrics"
+# returns data as pandas dataframe
 def read_dataset(filename):
     with open(filename, "rb") as file:
         return pickle.load(file)
