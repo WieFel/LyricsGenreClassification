c58474a5b0c73f91f666fd1c7b23361d2a08a89e
WieFel <felix.wielander@gmail.com>
2019-02-28 10:02:50 +0100

    Classification: Summing up word vectors for each lyrics

--- Classification.py
+++ Classification.py
@@ -1,22 +1,41 @@
-import os
+import os, pickle
 import numpy as np
 from gensim.models import Word2Vec
 
 
 DATA_PATH = os.path.expanduser("~/NLP_Data/")
+DATASET_FILE = DATA_PATH + "dataset.pickle"
 WORD2VEC_MODEL_FILE = DATA_PATH + "word2vec_model"
 
+DIMENSIONS = 300
+
+
+# returns data as pandas dataframe
+def read_dataset(filename):
+    with open(filename, "rb") as file:
+        return pickle.load(file)
+
+
+def sum_up_word_vecs(lyrics):
+    v = np.zeros((DIMENSIONS,))
+    for word in lyrics:
+        try:
+            v += model[word]
+        except:
+            pass
+    return v    # return sum vector
+
+
+# load dataset
+print("Reading dataset...")
+data = read_dataset(DATASET_FILE)
 
 # load our gensim
 model = Word2Vec.load(WORD2VEC_MODEL_FILE)
 
-# convert the wv word vectors into a numpy matrix that is suitable for insertion
-# into our TensorFlow and Keras models
-embedding_matrix = np.zeros((len(model.wv.vocab), 300)) # last parameter = dimension
-for i in range(len(model.wv.vocab)):
-    embedding_vector = model.wv[model.wv.index2word[i]]
-    if embedding_vector is not None:
-        embedding_matrix[i] = embedding_vector
+print(data)
 
+data["lyrics"] = data["lyrics"].map(sum_up_word_vecs)
 
-print(embedding_matrix)
+print("NEW DATA:")
+print(data)
