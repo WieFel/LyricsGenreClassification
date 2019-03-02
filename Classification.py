305fb4bbbab46c13aace2afc566245bd3075d59a
WieFel <felix.wielander@gmail.com>
2019-03-02 09:34:24 +0100

    Reading data using numpy

--- Classification.py
+++ Classification.py
@@ -1,15 +1,9 @@
-import pickle
+import numpy as np
 from FeatureExtraction import VECTORIZED_DATA
-
-
-# returns data as pandas dataframe
-def read_data(filename):
-    with open(filename, "rb") as file:
-        return pickle.load(file)
 
 
 # load dataset
 print("Reading vectorized data...")
-data = read_data(VECTORIZED_DATA)
+data = np.load(VECTORIZED_DATA)
 
 print(data)
